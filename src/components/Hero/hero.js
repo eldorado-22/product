e1a50f40a6e168dc1fb2990557e8a99b9f77eba8
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-02-26 00:27:23 +0600

    hero (#2)

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import {BsFillPlayFill} from "react-icons/bs";
+import family from './../../assets/img/family.png'
 
 const Hero = () => {
     return (
-        <section>
-
+        <section id="hero">
+            <div className="container">
+                <div className="hero">
+                    <div className="hero--text">
+                        <h1>Work at the speed <br/>
+                            of thought
+                        </h1>
+                        <p>Tools, tutorials, design and innovation experts,
+                            all <br/> in one place! The most intuitive way
+                            to imagine <br/> your next user experience.
+                        </p>
+                        <div className="hero--text__btn">
+                            <button>Get Started</button>
+                            <a href="https://www.youtube.com/watch?v=0s4lhbmO3IM"> <BsFillPlayFill className="hero--text__btn--play"/> Watch the Video</a>
+                        </div>
+                    </div>
+                    <img src={family} alt={family}/>
+                </div>
+            </div>
         </section>
     );
 };
