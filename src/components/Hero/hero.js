48b7548e6e29149b2efdeaf0735aae932cffa47e
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-02-26 14:28:32 +0600

    Hero (#3)
    * hero
    
    * hero

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -2,6 +2,24 @@ import React from 'react';
 import {BsFillPlayFill} from "react-icons/bs";
 import family from './../../assets/img/family.png'
 
+const Hero = ({dark}) => {
+    return (
+        <section style={{
+            background: dark ? 'rgba(36,100,88,0.95)' : '',
+            color: dark ? '#fff' : '',
+
+        }} id="hero">
+            <div className="container">
+                <div className="hero">
+                    <div className="hero--text">
+                        <h1 style={{
+                            color: dark ? '#fff' : ''
+                        }}>Work at the speed <br/>
+                            of thought
+                        </h1>
+                        <p style={{
+                            color: dark ? '#fff' : ''
+                        }}>Tools, tutorials, design and innovation experts,
 const Hero = () => {
     return (
         <section id="hero">
@@ -16,6 +34,15 @@ const Hero = () => {
                             to imagine <br/> your next user experience.
                         </p>
                         <div className="hero--text__btn">
+                            <button style={{
+                                background: dark ? '#fff' : '',
+                                color: dark ? '#02897A' : ''
+                            }}>Get Started
+                            </button>
+                            <a style={{
+                                color: dark ? '#fff' : ''
+                            }} href="https://www.youtube.com/watch?v=0s4lhbmO3IM"> <BsFillPlayFill
+                                className="hero--text__btn--play"/> Watch the Video</a>
                             <button>Get Started</button>
                             <a href="https://www.youtube.com/watch?v=0s4lhbmO3IM"> <BsFillPlayFill className="hero--text__btn--play"/> Watch the Video</a>
                         </div>
