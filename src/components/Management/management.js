39089e91e608c48f216b61f8ea484721659b97fb
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-02-26 17:28:26 +0600

    management (#5)

--- src/components/Management/management.js
+++ src/components/Management/management.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import management from './../../assets/img/management.png'
 
-const Management = () => {
+const Management = ({dark}) => {
     return (
-        <div>
-            
-        </div>
+        <section style={{
+            background: dark ? 'rgba(36,100,88,0.95)' : '',
+            color: dark ? '#fff' : ''
+        }} id="management">
+            <div className="container">
+                <div className="management">
+                    <div className="management--text">
+                        <ul style={{
+                            color: dark ? '#fff' : ''
+                        }}>Effortless Validation for</ul>
+                        <h2 style={{
+                            color: dark ? '#fff' : ''
+                        }}>Management</h2>
+                        <p style={{
+                            color: dark ? '#fff' : ''
+                        }} className="management--text__para">The Myspace page defines the individual,
+                            his or her <br/>characteristics, traits,
+                            personal choices and the overall <br/>
+                            personality of the person.
+                        </p>
+
+                        <ul style={{
+                            color: dark ? '#fff' : ''
+                        }} className="management--text__accessory">Accessory makers</ul>
+                        <p style={{
+                            color: dark ? '#fff' : ''
+                        }}>While most people enjoy casino gambling,
+                            sports betting, <br/>lottery and bingo
+                            playing for the fun.
+                        </p>
+
+                        <ul style={{
+                            color: dark ? '#fff' : ''
+                        }} className="management--text__alter">Alterationists</ul>
+                        <p style={{
+                            color: dark ? '#fff' : ''
+                        }} >If you are looking for a new way to
+                            promote your business <br/> that
+                            won’t cost you more money.
+                        </p>
+                    </div>
+                    <img src={management} alt={management}/>
+                </div>
+            </div>
+        </section>
     );
 };
 
