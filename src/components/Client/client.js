bb5501f16254a9aa1dec7d865a49df5e33e3cde2
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-04-09 07:11:27 +0600

    signIn (#11)

---
+++ src/components/Client/client.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './client.scss';
+import woman from "./../../assets/img/woman.svg"
+import boy from "./../../assets/img/boy.svg"
+import man from "./../../assets/img/man.svg"
+
+const Client = () => {
+    return (
+        <section id="client">
+            <div className="container">
+                <div className="client">
+                    <h1>What Clients Say</h1>
+
+                    <p>Problems trying to resolve the conflict between <br/>
+                        the two major realms of Classical physics:
+                        Newtonian mechanics
+                    </p>
+
+                    {/*<div className="client--block">*/}
+                    {/*    <div className="client--block__one">*/}
+                    {/*        <div className="client--block__one--star">*/}
+                    {/*            <div className="client--block__one--star__two"></div>*/}
+                    {/*            <div className="client--block__one--star__two"></div>*/}
+                    {/*            <div className="client--block__one--star__two"></div>*/}
+                    {/*            <div className="client--block__one--star__two"></div>*/}
+                    {/*        </div>*/}
+                    {/*        <p>Product helps you see how many <br/>*/}
+                    {/*            more days you need to work to <br/>*/}
+                    {/*            reach your financial goal.*/}
+                    {/*        </p>*/}
+                    {/*        <div className="client--block__one--woman">*/}
+                    {/*            <img src={woman} alt="img"/>*/}
+                    {/*            <h6>Wahid Ari <br/>*/}
+                    {/*                <span>Designer</span>*/}
+                    {/*            </h6>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Client;
