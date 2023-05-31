606b86a796a940b588a222f793a38b4e6dc3c23d
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-05-31 04:02:21 +0300

    clients-blocks-pages (#17)

--- src/components/Block/block.js
+++ src/components/Block/block.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import "./style.scss";
+import block from "./../../assets/img/block.png";
 
 const Block = () => {
     return (
-        <div>
-
-        </div>
+        <section id="join">
+            <div className="container">
+                <div className="join">
+                  <div className="join--text">
+                      <h2>Join 100 Compannies who boost their business with <br/>
+                          Product
+                      </h2>
+                      <button>get this</button>
+                  </div>
+                    <img src={block} alt="img"/>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Client/client.js
+++ src/components/Client/client.js
@@ -3,43 +3,63 @@ import './client.scss';
 import woman from "./../../assets/img/woman.svg"
 import boy from "./../../assets/img/boy.svg"
 import man from "./../../assets/img/man.svg"
-
+import stars from "./../../assets/img/stars.svg"
 const Client = ({dark}) => {
     return (
         <section id="client">
             <div className="container">
                 <div className="client">
-                    {/*<h1 style={{*/}
-                    {/*    color: dark ? '#fff' : '',*/}
-                    {/*}}>What Clients Say</h1>*/}
-                    {/**/}
-                    {/*<p style={{*/}
-                    {/*    color: dark ? '#fff' : '',*/}
-                    {/*}}>Problems trying to resolve the conflict between <br/>*/}
-                    {/*    the two major realms of Classical physics:*/}
-                    {/*    Newtonian mechanics*/}
-                    {/*</p>*/}
+                    <h1 style={{
+                        color: dark ? '#fff' : '',
+                    }}>What Clients Say</h1>
 
-                    {/*<div className="client--block">*/}
-                    {/*    <div className="client--block__one">*/}
-                    {/*        <div className="client--block__one--star">*/}
-                    {/*            <div className="client--block__one--star__two"></div>*/}
-                    {/*            <div className="client--block__one--star__two"></div>*/}
-                    {/*            <div className="client--block__one--star__two"></div>*/}
-                    {/*            <div className="client--block__one--star__two"></div>*/}
-                    {/*        </div>*/}
-                    {/*        <p>Product helps you see how many <br/>*/}
-                    {/*            more days you need to work to <br/>*/}
-                    {/*            reach your financial goal.*/}
-                    {/*        </p>*/}
-                    {/*        <div className="client--block__one--woman">*/}
-                    {/*            <img src={woman} alt="img"/>*/}
-                    {/*            <h6>Wahid Ari <br/>*/}
-                    {/*                <span>Designer</span>*/}
-                    {/*            </h6>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <p style={{
+                        color: dark ? '#fff' : '',
+                    }}>Problems trying to resolve the conflict between <br/>
+                        the two major realms of Classical physics:
+                        Newtonian mechanics
+                    </p>
+                    <div className="client--block">
+                        <div className="client--block__one">
+                            <img src={stars} alt="img" className="images"/>
+                            <p>Product helps you see how many <br/>
+                                more days you need to work to <br/>
+                                reach your financial goal.
+                            </p>
+                            <div className="client--block__one--woman">
+                                <img src={woman} alt="img"/>
+                                <h6>Wahid Ari <br/>
+                                    <span>Designer</span>
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="client--block__one">
+                            <img src={stars} alt="img" className="images"/>
+                            <p>Product helps you see how many <br/>
+                                more days you need to work to <br/>
+                                reach your financial goal.
+                            </p>
+                            <div className="client--block__one--woman">
+                                <img src={boy} alt="img"/>
+                                <h6>Wahid Ari <br/>
+                                    <span>Designer</span>
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="client--block__one">
+                            <img src={stars} alt="img" className="images"/>
+                            <p>Product helps you see how many <br/>
+                                more days you need to work to <br/>
+                                reach your financial goal.
+                            </p>
+                            <div className="client--block__one--woman">
+                                <img src={man} alt="img"/>
+                                <h6>Wahid Ari <br/>
+                                    <span>Designer</span>
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
